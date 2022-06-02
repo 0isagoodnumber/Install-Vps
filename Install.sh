64a1a2a58ef38cbb7b09afc6a989497a2588259b
Pedro Ferreira <66525685+PedroFerreira97@users.noreply.github.com>
2022-06-02 21:30:18 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,6 +1,6 @@
 
 echo 'update and upgrade repository'
-sudo apt-add-repository ppa:fish-shell/release-3
+#sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get upgrade
 #sudo apt-get install fish
